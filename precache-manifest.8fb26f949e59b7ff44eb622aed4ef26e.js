7cbd8a574f8b6c855026c9eab7e688836f45e861
mnescot <mnescot@gmail.com>
2020-07-26 23:54:58 +0000

    Updates

---
+++ precache-manifest.8fb26f949e59b7ff44eb622aed4ef26e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c5b1c1770c5437c6e219ea9182b4c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd5569db8d5329e9bdf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "837ced84e69c3d6739ec",
+    "url": "/static/js/2.db443d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db443d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd5569db8d5329e9bdf",
+    "url": "/static/js/main.d8052b35.chunk.js"
+  },
+  {
+    "revision": "325a53b16c523b4f884d",
+    "url": "/static/js/runtime-main.a5a4b352.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-dev/precache-manifest.854e98399519c1b5d4ecfa0a5d0ce33f.js"
+  "/precache-manifest.8fb26f949e59b7ff44eb622aed4ef26e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
