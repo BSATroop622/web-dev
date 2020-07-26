f8abd584474e4b850e9b565f0edf27c18037e165
mnescot <mnescot@gmail.com>
2020-07-26 23:18:24 +0000

    Updates

---
+++ precache-manifest.854e98399519c1b5d4ecfa0a5d0ce33f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d53e3d5ddd7619afb26fef74816189a",
+    "url": "/web-dev/index.html"
+  },
+  {
+    "revision": "d56ea00766cdb710ac77",
+    "url": "/web-dev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "837ced84e69c3d6739ec",
+    "url": "/web-dev/static/js/2.db443d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-dev/static/js/2.db443d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56ea00766cdb710ac77",
+    "url": "/web-dev/static/js/main.08ba985e.chunk.js"
+  },
+  {
+    "revision": "60fb10540d2ba2fe4452",
+    "url": "/web-dev/static/js/runtime-main.175069d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
